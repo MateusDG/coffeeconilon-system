f89970fd4a104bab27b663f98029258c2b7a90a8
Mateus <mateusdinizgo@hotmail.com>
2025-07-01 14:11:33 -0300

    Refactor code structure for improved readability and maintainability

--- backend/app/models/lot.py
+++ backend/app/models/lot.py
@@ -4,23 +4,23 @@ from app.core.database import Base
 from .mixins import TimestampMixin
 
 class Lot(TimestampMixin, Base):
-    __tablename__ = "lotes"
+    __tablename__ = "lots"
 
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String(80), nullable=False)
+    name = Column(String(80), nullable=False)
     area_ha = Column(Float, nullable=False)  # hectares
     farm_id = Column(Integer, ForeignKey("farms.id"), nullable=False)
-    crop_year = Column(Integer)  # safra 2025, 2026...
+    crop_year = Column(Integer)
 
-    fazenda = relationship("Farm", back_populates="lotes")
-    culturas = relationship("Crop", back_populates="lote", cascade="all, delete")
+    farm = relationship("Farm", back_populates="lots")
+    crops = relationship("Crop", back_populates="lot", cascade="all, delete")
     financial_records = relationship(
-        "Financeiro",
-        back_populates="lote",
+        "Financial",
+        back_populates="lot",
         cascade="all, delete",
     )
-    acoes = relationship(
-        "Estoque",
-        back_populates="lote",
+    stocks = relationship(
+        "Stock",
+        back_populates="lot",
         cascade="all, delete",
     )
